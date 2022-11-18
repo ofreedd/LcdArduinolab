58809474caf606459b1c0f0c68922017796a9352
Fred Alves <117871681+ofreedd@users.noreply.github.com>
2022-11-18 17:21:47 -0300

    Update copy_of_arduino_lcd_game1.ino

--- copy_of_arduino_lcd_game1.ino
+++ copy_of_arduino_lcd_game1.ino
@@ -8,7 +8,7 @@
 #define SPRITE_RUN1 1
 #define SPRITE_RUN2 2
 #define SPRITE_JUMP 3
-#define SPRITE_JUMP_UPPER '.'         // Use the '.' character for the head
+#define SPRITE_JUMP_UPPER '.'         // Use the'.'character for the head
 #define SPRITE_JUMP_LOWER 4
 #define SPRITE_TERRAIN_EMPTY ' '      // User the ' ' character
 #define SPRITE_TERRAIN_SOLID 5
